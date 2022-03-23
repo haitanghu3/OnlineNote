43292103cb6aed9ec67c6e055196a37b4d5b0e30
落影汐雾 <1838001290@qq.com>
2022-03-23 14:51:10 +0800

    add:onlineNote_backend change:README.md

---
+++ onlineNote_backend/src/main/java/com/werun/onlinenote_backend/OnlineNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.werun.onlinenote_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineNoteBackendApplication.class, args);
+    }
+
+}
